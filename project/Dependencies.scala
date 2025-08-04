1e9bfc9b14acd69b6ae28fdb4925218fc2bc9b9c
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2025-08-04 12:18:55 +0100

    [CIR-2151] Utilise api-test-runner lib

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,17 +2,10 @@ import sbt._
 
 object Dependencies {
 
-  private val playWsVersion = "3.0.7"
-
   val test: Seq[ModuleID] = Seq(
-    "org.scalatest"                %% "scalatest"                % "3.2.19",
-    "com.vladsch.flexmark"          % "flexmark-all"             % "0.64.8",
-    "com.typesafe"                  % "config"                   % "1.4.3",
-    "org.playframework"            %% "play-ahc-ws-standalone"   % playWsVersion,
-    "org.playframework"            %% "play-ws-standalone-json"  % playWsVersion,
-    "org.wiremock"                  % "wiremock"                 % "3.13.1",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"     % "2.19.2",
-    "io.swagger.parser.v3"          % "swagger-parser"           % "2.1.31",
-    "org.openapi4j"                 % "openapi-schema-validator" % "1.0.7"
+    "uk.gov.hmrc"         %% "api-test-runner"          % "0.10.0",
+    "org.wiremock"         % "wiremock"                 % "3.13.1",
+    "io.swagger.parser.v3" % "swagger-parser"           % "2.1.31",
+    "org.openapi4j"        % "openapi-schema-validator" % "1.0.7"
   ).map(_ % Test)
 }
