56aa412fb48492dc70f3c844f0658d7a2219bd0c
grahampaulcook <11269301+grahampaulcook@users.noreply.github.com>
2026-04-27 12:18:48 +0100

    CIR-2834 Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,8 @@ object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"         %% "api-test-runner"          % "0.10.0",
-    "org.wiremock"         % "wiremock"                 % "3.13.1",
-    "io.swagger.parser.v3" % "swagger-parser"           % "2.1.31",
+    "org.wiremock"         % "wiremock"                 % "3.13.2",
+    "io.swagger.parser.v3" % "swagger-parser"           % "2.1.40",
     "org.openapi4j"        % "openapi-schema-validator" % "1.0.7"
   ).map(_ % Test)
 }
